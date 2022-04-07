92b807faeed2e92aeb701304d91c3c69566a77f3
Georgi Dinov <georgidinov1@abv.bg>
2022-04-07 08:44:34 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pu_ads_software_engineering_spring_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
